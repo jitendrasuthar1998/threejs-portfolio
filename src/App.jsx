710c6ec3f5971749f7f0152235c94e99cc703727
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2024-11-21 21:58:48 +0530

    added responsive NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Navbar from "./sections/Navbar"
 
 function App() {
   return (
     <main className="max-w-7xl mx-auto">
-      <h1 className="text-white text-2xl">Hello Three JS</h1>
+      <Navbar/>
     </main>
   )
 }
